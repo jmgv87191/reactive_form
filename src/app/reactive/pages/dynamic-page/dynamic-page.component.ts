32f63e2ea32f647edb73a01df0820e2671863dc7
juan manuel <jmgv87191@gmail.com>
2025-01-30 14:41:07 -0700

    ad

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -11,10 +11,12 @@ import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } fr
 export class DynamicPageComponent {
 
   form:FormGroup
+  newFavorite: FormControl
 
   constructor( 
       private fb:FormBuilder 
   ){
+
     this.form = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       favoriteGames: this.fb.array([
@@ -23,23 +25,49 @@ export class DynamicPageComponent {
         
       ])
     })
+
+    this.newFavorite = this.fb.control('', Validators.required)
+
   }
 
   get favoriteGames (){
-    
     return this.form.get('favoriteGames') as FormArray ;
+  }
+
+
+  isValidFormArray( formArray: FormArray, i:number ){
+    return this.form.controls[ i ].errors
+    return this.form.controls[ i ].touched
   }
 
   onSubmit(){
 
     if (this.form.invalid) {
-      
-      this.form.markAllAsTouched();
+          this.form.markAllAsTouched();
       return;
     }
     console.log(this.form.value)
     this.form.reset();
   }
 
+  onDeleteFavorite( index:number ){
+    this.favoriteGames.removeAt(index)
+  }
+
+  onAddToFavorites(){
+
+    if (this.newFavorite.invalid) {
+      return;
+    }
+
+    const newGame = this.newFavorite.value
+
+    this.favoriteGames.push( 
+      this.fb.control(newGame, Validators.required)
+    )
+
+    this.newFavorite.reset();
+
+  }
 
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,12 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
   styleUrl: './switches-page.component.css'
 })
 export class SwitchesPageComponent {
+
+  public myForm: FormGroup;
+
+  constructor( private fb: FormBuilder ){
+    
+    this.myForm = this.fb.group({
+      gender:['M', Validators.required],
+      wantNotifications:[true, Validators.required],
+      termsAndConditions:[false, Validators.requiredTrue]
+    })
+
+  }
+
+  isValidField( field: string ){
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+
+  onSave(){
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 
 }
